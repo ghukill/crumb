31bc724aabf5116b9cdb440bc44a5282e0013ef0
Graham Hukill <ghukill@gmail.com>
2015-03-25 14:45:46 -0700

    added kafka tests

--- crumb_tests.py
+++ crumb_tests.py
@@ -6,11 +6,20 @@
 import random
 import requests
 import sys
+import json
+
+import localConfig
 
 # crumbDB
 from crumbDB import models, utilities
 
-import localConfig
+# Apache Kafka
+from kafka import KafkaClient, SimpleProducer
+
+# init Kafka
+kafka = KafkaClient("localhost:9092")
+producer = SimpleProducer(kafka)
+
 
 
 ##### Raw crumbDB Tests #####
@@ -70,6 +79,63 @@ def run_crumbDB_tests():
 	getNumRange(start,end,index)
 	updateNumRange(start,end,index,new_value)
 	deleteNumRange(start,end,index)
+
+	print "Finished range tests with these params:",start,end,new_value,index
+
+
+##### Raw crumbDB through Apache Kafka Tests #####
+
+@utilities.timing
+def kafka_writeNumRange(start, end, index):
+	'''
+	arguments: (start, end, index), where "start" and "end" as integers for both key and value, and "index" as new index
+	'''
+	for x in range(start,end):
+		result = producer.send_messages("crumb_air", json.dumps( { "action":"write", "key":x, "value":x, "index":index } ))
+
+
+
+@utilities.timing
+def kafka_getNumRange(start, end, index):
+	'''
+	arguments: (start, end, index), where "start" and "end" as integers for both key and value, and "index" as new index
+	'''
+	for x in range(start,end):
+		result = producer.send_messages("crumb_air", json.dumps( { "action":"get", "key":x, "value":x, "index":index } ))
+
+
+
+@utilities.timing
+def kafka_updateNumRange(start, end, index, new_value):
+	'''
+	arguments: (start, end, index), where "start" and "end" as integers for key, "new_value" as new value, and "index" as new index
+	'''
+	for x in range(start,end):
+		result = producer.send_messages("crumb_air", json.dumps( { "action":"update", "key":x, "value":new_value, "index":index } ))
+
+
+
+@utilities.timing
+def kafka_deleteNumRange(start, end, index):
+	'''
+	arguments: (start, end, index), where "start" and "end" as integers for key, "new_value" as new value, and "index" as new index
+	'''
+	for x in range(start,end):
+		result = producer.send_messages("crumb_air", json.dumps( { "action":"delete", "key":x, "value":x, "index":index } ))
+
+
+@utilities.timing
+def run_kafka_tests():
+	
+	start = 1
+	end = 500
+	new_value = 'updated'
+	index = str(int(random.random() * 100000))
+
+	kafka_writeNumRange(start,end,index)
+	kafka_getNumRange(start,end,index)
+	kafka_updateNumRange(start,end,index,new_value)
+	kafka_deleteNumRange(start,end,index)
 
 	print "Finished range tests with these params:",start,end,new_value,index
 
@@ -191,11 +257,15 @@ if __name__ == '__main__':
 			print "Running crumbDB tests"
 			run_crumbDB_tests()
 
+		if perform in ['kafka','all']:
+			print "Running kafka tests"
+			run_kafka_tests()
+
 		if perform in ['crumb_http','all']:
 			print "Running crumb_http GET tests"
 			run_crumb_http_tests()
 
 	else:
-		print "Please type either 'crumbDB', 'crumb_http', or 'all'"
+		print "Please type either 'crumbDB', 'kafka', crumb_http', or 'all'"
 
 	
